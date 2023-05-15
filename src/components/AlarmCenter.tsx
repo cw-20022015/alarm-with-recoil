fcf872f9f50217532f88cda58bc9b6865807c4d6
jungjihyoun <jhjung@coway.com>
2023-05-15 17:25:27 +0900

    feat(CWF-39) : 알람센터(알람리스트) 컴포넌트

---
+++ src/components/AlarmCenter.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { useRecoilValue } from 'recoil';
+import { alarmListByDateSelector } from 'store/AlarmSelector';
+import dayjs from 'dayjs';
+import { Alarm } from 'types/alarm.types';
+
+function AlarmCenter() {
+  //   TODO : 알림센터 팝업 누르면 알림 리스트 뿌려주기 (selector 에서 날짜순으로 정렬하여 가져온다)
+  const alarmList = useRecoilValue(alarmListByDateSelector);
+
+  return (
+    <div className="DashBoard">
+      <div>푸쉬 알림창</div>
+      <div>
+        {alarmList.map((e: Alarm) => (
+          <div key={e.id}>
+            <div>{dayjs(e.date).format('YYYY-MM-DD')}</div>
+            <div>{e.content}</div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default AlarmCenter;
