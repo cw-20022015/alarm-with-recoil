6bbc97e8b3ca48a04f72739f20e20148788ce3a9
정지현[프론트엔드개발팀] <132965046+cw-20022015@users.noreply.github.com>
2023-06-21 15:45:48 +0900

    feat: intl init

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import { IntlProvider } from 'react-intl';
+import { en, kr } from 'locales';
 import DashBoard from './page/DashBoard';
+
 import './App.css';
 
 function App() {
-  return <DashBoard />;
+  const { language } = window.navigator;
+  const messages = { 'en-US': en, ko: kr }[language];
+
+  return (
+    // locale : 앱이 렌더링 되는 시점의 언어
+    // messages : 각각 언어와 json 파일을 매칭
+    // defaultLocale : 브라우저 언어랑 일치해야 함
+    <IntlProvider messages={messages} locale={language} defaultLocale={language}>
+      <DashBoard />;
+    </IntlProvider>
+  );
 }
 
 export default App;
---
+++ src/locales/index.ts
@@ -0,0 +1,7 @@
+import * as localeKr from './kr.json';
+import * as localeEn from './en.json';
+
+export const kr = localeKr;
+export const en = localeEn;
+
+export default { kr, en };
