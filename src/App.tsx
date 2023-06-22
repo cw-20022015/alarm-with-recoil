d58e6300845ebe825ffb1592b2daf43e9cc1a7be
정지현[프론트엔드개발팀] <132965046+cw-20022015@users.noreply.github.com>
2023-06-22 11:13:04 +0900

    feat: 언어 선택하여 번역

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { IntlProvider } from 'react-intl';
 import { en, kr } from 'locales';
 import DashBoard from './page/DashBoard';
@@ -7,14 +7,15 @@ import './App.css';
 
 function App() {
   const { language } = window.navigator;
-  const messages = { 'en-US': en, ko: kr }[language];
+  const [localLanguage, setLocalLanguage] = useState(language);
+  const messages = { ko: kr, 'en-US': en }[localLanguage];
 
   return (
     // locale : 앱이 렌더링 되는 시점의 언어
     // messages : 각각 언어와 json 파일을 매칭
-    // defaultLocale : 브라우저 언어랑 일치해야 함
-    <IntlProvider messages={messages} locale={language} defaultLocale={language}>
-      <DashBoard />;
+    // defaultLocale : 브라우저 언어랑 일치
+    <IntlProvider messages={messages} locale={localLanguage} defaultLocale={localLanguage}>
+      <DashBoard localLanguage={localLanguage} handleLocalLanguage={setLocalLanguage} />;
     </IntlProvider>
   );
 }
--- src/page/DashBoard.tsx
+++ src/page/DashBoard.tsx
@@ -6,11 +6,26 @@ import PushAlarm from 'components/PushAlarm';
 import AlarmCenter from 'components/AlarmCenter/AlarmCenter';
 import AlarmCenterButton from 'components/AlarmCenter/AlarmCenterButton';
 
-function DashBoard() {
+type DashBoard = {
+  localLanguage: string;
+  handleLocalLanguage: React.Dispatch<React.SetStateAction<string>>;
+};
+
+function DashBoard(props: DashBoard) {
+  const { handleLocalLanguage, localLanguage } = props;
   const isAlarmCenterOpen = useRecoilValue(isAlarmCenterOpenState);
 
   return (
     <StyledDashBoard>
+      <StyledRadioGroup>
+        <StyledLabel htmlFor="English">
+          <StyledRadio type="checkbox" id="en-US" name="en-US" value="en-US" onClick={() => handleLocalLanguage('en-US')} checked={localLanguage === 'en-US'} /> <span>English</span>
+        </StyledLabel>
+        <StyledLabel htmlFor="Korean">
+          <StyledRadio type="checkbox" id="ko" name="ko" value="ko" onClick={() => handleLocalLanguage('ko')} checked={localLanguage === 'ko'} /> <span>Korean</span>
+        </StyledLabel>
+      </StyledRadioGroup>
+
       <StyledPushAlarmSection>
         <PushAlarm />
         <PushAlarm />
@@ -34,6 +49,52 @@ const StyledDashBoard = styled.div`
 const StyledPushAlarmSection = styled.div`
   display: flex;
   flex-direction: column;
+`;
+
+const StyledRadioGroup = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const StyledLabel = styled.label`
+  display: flex;
+  align-items: center;
+
+  :hover {
+    cursor: pointer;
+  }
+
+  > span {
+    padding: 0;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 20px;
+    letter-spacing: -0.02em;
+  }
+`;
+
+const StyledRadio = styled.input.attrs(() => ({
+  type: 'radio',
+}))`
+  appearance: none;
+  margin: 0 11px 0 0;
+  width: 18px;
+  height: 18px;
+  border: 1.5px solid #e2e2e2;
+  border-radius: 50%;
+
+  :hover {
+    cursor: pointer;
+  }
+
+  &:checked {
+    background: #e2e2e2;
+    border: none;
+
+    :disabled {
+      background: #e2e2e2;
+    }
+  }
 `;
 
 export default DashBoard;
